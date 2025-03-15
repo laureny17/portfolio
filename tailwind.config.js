784db72f07e5363fdf2a04cfa57f2ccc1d60b1bf
Lauren Yoo <laureny@mit.edu>
2025-03-15 19:06:42 -0400

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        black: 'var(--black)',
+        accent: 'var(--accent)',
+        gray: 'var(--gray)',
+        lightgray: 'var(--light-gray)',
+      },
+      fontFamily: {
+        title: ['Pecita'],
+        body: ['Reddit Mono'],
+      },
+    },
+  },
+}
