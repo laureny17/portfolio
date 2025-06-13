2ef5088b493b5b7fa0a8f5d078fd42659410a536
Lauren Yoo <laurenyoo317@gmail.com>
2025-06-13 13:17:55 -0400

    updated portfolio title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import type React from "react";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Lauren Yoo",
+  description: "Personal portfolio",
+};
 
 export default function RootLayout({
   children,
